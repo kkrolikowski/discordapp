9ef7232ec3b8bd06970e899ebbb6d57e67167426
Krzysztof Królikowski <kkrolikowski@gmail.com>
2022-05-24 23:25:22 +0200

    adding reference to datadir

--- lib/edziennik.py
+++ lib/edziennik.py
@@ -2,7 +2,8 @@ from pickle import PicklingError
 
 
 class Edziennik:
-    def __init__(self, **kwargs):
+    def __init__(self, datadir, **kwargs):
         self.token = kwargs["vulcan_token"]
         self.symbol = kwargs["vulcan_symbol"]
         self.pin = kwargs["vulcan_pin"]
+        self.datadir = datadir
--- run.py
+++ run.py
@@ -7,10 +7,12 @@ from lib.edziennik import Edziennik
 cfg = configparser.ConfigParser()
 cfg.read(os.path.join(os.path.dirname(__file__), 'conf/app.conf'))
 
-edziennik = Edziennik(**cfg["VULCAN"])
+datadir = os.path.join(os.path.dirname(__file__), "data")
+edziennik = Edziennik(datadir, **cfg["VULCAN"])
 print(edziennik.pin)
 print(edziennik.symbol)
 print(edziennik.token)
+print(edziennik.datadir)
 
 discord_token = cfg["MAIN"]["discord_token"]
 bot = DiscordBot()
