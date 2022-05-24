412a24467b01633f733fa44771ea466579f442d3
Krzysztof Królikowski <kkrolikowski@gmail.com>
2022-05-24 23:51:12 +0200

    runtime error fix

--- lib/edziennik.py
+++ lib/edziennik.py
@@ -2,8 +2,8 @@ from pickle import PicklingError
 
 
 class Edziennik:
-    def __init__(self, credentials_dir, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.token = kwargs["vulcan_token"]
         self.symbol = kwargs["vulcan_symbol"]
         self.pin = kwargs["vulcan_pin"]
-        self.credentials_dir = credentials_dir
+        self.credentials_dir = args[0]
