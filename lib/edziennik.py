f6e5d52671264c9dd3acbae2aa8ee80eeff1340d
Krzysztof Królikowski <kkrolikowski@gmail.com>
2022-05-24 23:20:41 +0200

    runtime fix

--- lib/edziennik.py
+++ lib/edziennik.py
@@ -3,6 +3,6 @@ from pickle import PicklingError
 
 class Edziennik:
     def __init__(self, **kwargs):
-        self.token = kwargs.vulcan_token
-        self.symbol = kwargs.vulcan_symbol
-        self.pin = kwargs.vulcan_pin
+        self.token = kwargs["vulcan_token"]
+        self.symbol = kwargs["vulcan_symbol"]
+        self.pin = kwargs["vulcan_pin"]
