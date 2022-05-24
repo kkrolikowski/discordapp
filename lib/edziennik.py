ddf0da02a512cd37d47d09ce0b2262a4b869d409
Krzysztof Królikowski <kkrolikowski@gmail.com>
2022-05-24 23:49:09 +0200

    switch back to single variable

--- lib/edziennik.py
+++ lib/edziennik.py
@@ -2,8 +2,8 @@ from pickle import PicklingError
 
 
 class Edziennik:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, credentials_dir, **kwargs):
         self.token = kwargs["vulcan_token"]
         self.symbol = kwargs["vulcan_symbol"]
         self.pin = kwargs["vulcan_pin"]
-        self.credentials_dir = args[0]
+        self.credentials_dir = credentials_dir
