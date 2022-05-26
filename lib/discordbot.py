e12d2e9e2e665c2e74d4ee79bf5b54a3ef0f3762
Krzysztof Królikowski <kkrolikowski@gmail.com>
2022-05-26 07:42:52 +0200

    feat: help instruction

--- lib/discordbot.py
+++ lib/discordbot.py
@@ -13,7 +13,9 @@ class DiscordBot(discord.Client):
         if message.content.startswith('$register'):
             arguments = message.content.split(" ")
             if len(arguments) < 4:
-                await message.channel.send("Aby zarejestrować aplikację w edzienniku musisz podać: Token, Miasto i PIN")
+                await message.channel.send("""Aby zarejestrować aplikację w edzienniku musisz zalogować się w przeglądarce do systemu Vulcan i 
+                                            wygenerować nowy dostęp mobilny. Na spodzie kodu QR znajdziesz informacjie: Token, Symbol i PIN.
+                                            Musisz je kolejno po spacji wpisać: $register token symbol pin""")
             else:
                 await message.channel.send("Rejestruję konto w edzienniku dla " + str(message.author.nick))
                 await message.channel.send("Token: " + arguments[1] + ", Miasto: " + arguments[2] + ", PIN: " + arguments[3])
